3b6a186f55cfe217f33b1b7836e2c9cdca43f10e
Cian O'Gralaigh <ihatecian@gmail.com>
2018-09-27 00:20:57 +0100

    Add navbar component skeleton, add jumbosection component, add particle bg, style jumbosection

---
+++ src/components/jumbotron.jsx
@@ -0,0 +1,146 @@
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+
+export default class Jumbosection extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="text-center  ">
+        <div className="container">
+          <Particles
+            className="jumbo-animation"
+            params={{
+              particles: {
+                number: {
+                  value: 80,
+                  density: {
+                    enable: true,
+                    value_area: 800
+                  }
+                },
+                color: {
+                  value: "#b9b1a6"
+                },
+                shape: {
+                  type: "circle",
+                  stroke: {
+                    width: 0,
+                    color: "#000000"
+                  },
+                  polygon: {
+                    nb_sides: 5
+                  },
+                  image: {
+                    src: "img/github.svg",
+                    width: 100,
+                    height: 100
+                  }
+                },
+                opacity: {
+                  value: 0.5,
+                  random: false,
+                  anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false
+                  }
+                },
+                size: {
+                  value: 12.02559045649142,
+                  random: true,
+                  anim: {
+                    enable: true,
+                    speed: 9.744926547616142,
+                    size_min: 0.1,
+                    sync: true
+                  }
+                },
+                line_linked: {
+                  enable: true,
+                  distance: 150,
+                  color: "#ffffff",
+                  opacity: 0.4,
+                  width: 1
+                },
+                move: {
+                  enable: true,
+                  speed: 6,
+                  direction: "top-right",
+                  random: true,
+                  straight: true,
+                  out_mode: "out",
+                  bounce: false,
+                  attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200
+                  }
+                }
+              },
+              interactivity: {
+                detect_on: "canvas",
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "grab"
+                  },
+                  onclick: {
+                    enable: true,
+                    mode: "push"
+                  },
+                  resize: true
+                },
+                modes: {
+                  grab: {
+                    distance: 400,
+                    line_linked: {
+                      opacity: 1
+                    }
+                  },
+                  bubble: {
+                    distance: 400,
+                    size: 40,
+                    duration: 2,
+                    opacity: 8,
+                    speed: 3
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4
+                  },
+                  push: {
+                    particles_nb: 4
+                  },
+                  remove: {
+                    particles_nb: 2
+                  }
+                }
+              },
+              retina_detect: true
+            }}
+          />{" "}
+        </div>
+        <div className="jumbotron" id="jumbo">
+          <div className="overlay container">
+            <div className="jumbo-heading">
+              <h1 className="display-4">
+                Hello I'm <b className="jumbo-name">Cian O'Gralaigh</b> <br />
+                and I'm a full-stack web developer.
+              </h1>
+            </div>
+            <div className="jumbo-btn-div">
+              <button
+                type="button"
+                className="btn btn-success"
+                id="jumbotron-button"
+              >
+                See My Work
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+
+export default class Navsection extends React.Component {
+  // Sets the class as the default export
+
+  constructor(props) {
+    // Call component constructor function and reinitialise it
+    super(props);
+
+    // Bind the toggle function to this object so it can be used outside of the class
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      // Initialise the state
+      isOpen: false
+    };
+  }
+  toggle() {
+    // Accesses the bound object and updates its state prop
+
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Cian O'Gralaigh</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="#">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Projects</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Contact</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
